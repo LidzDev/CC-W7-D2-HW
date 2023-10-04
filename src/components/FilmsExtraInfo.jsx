de77a083731ea7c26b66ab0197eba0e483c9b5e6
LidzDev <lydia.cordell@gmail.com>
2023-10-04 09:54:05 +0100

    fixed the angle brackets

--- src/components/FilmsExtraInfo.jsx
+++ src/components/FilmsExtraInfo.jsx
@@ -2,8 +2,8 @@ const FilmExtraInfo = () => {
 
     return (
         <>
-        <h2><a href="https://www.imdb.com/calendar/?region=gb">View more upcoming releases >></a></h2>
-        </>
+            <h2><a href="https://www.imdb.com/calendar/?region=gb">View more upcoming releases {">>"} </a></h2>
+        </> 
     )
 }
 
